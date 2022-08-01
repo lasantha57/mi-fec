7379c4653c324ef4702b4f45d0cee8a59ba3adfc
Lasantha <lasantha57@gmail.com>
2022-08-01 22:03:11 +0530

    Add snapshot tests for VideoList

---
+++ src/pages/video/VideoList/__tests__/VideoList.test.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import { cleanup } from '@testing-library/react';
+
+import { VideoList } from '..';
+
+afterEach(cleanup)
+
+describe('Testing VideoList', () => {
+    it('Component should render correctly', () => {
+        const tree = renderer
+            .create(
+                <Router>
+                    <VideoList />
+                </Router>
+            )
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
